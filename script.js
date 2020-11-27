23d501ad71177f74f150d470c13d0cc8a7e7b4cf
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2020-11-26 20:17:34 -0500

    Fixed error in JS, updated contact links to open in new tab

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-$("#portfolio-link").on("mouseenter", function(e){
+$("#portfolio-link").on("mouseenter", function(){
     $("#portfolio-text").text("PORTFOLIO");
     $("#portfolio-text").css("margin-left", "13%");
     $("#portfolio-text").css("opacity", "50%");
 });
 
-$("#portfolio-link").on("mouseleave", function(e){
+$("#portfolio-link").on("mouseleave", function(){
     $("#portfolio-text").text("ETHAN");
     $("#portfolio-text").css("margin-left", "0");
     $("#portfolio-text").css("opacity", "100%");
 });
 
-$("#contact-link").on("mouseenter", function(e){
+$("#contact-link").on("mouseenter", function(){
     $("#contact-text").text("CONTACT");
     $("#contact-text").css("margin-left", "13%");
     $("#contact-text").css("opacity", "50%");
@@ -28,7 +28,7 @@ $("#resume-link").on("mouseenter", function(){
     $("#resume-text").css("opacity", "50%");
 });
 
-$("#resume-link").on("mouseleave", function(e){
+$("#resume-link").on("mouseleave", function(){
     $("#resume-text").text("MCDOWELL");
     $("#resume-text").css("margin-left", "0");
     $("#resume-text").css("opacity", "100%");
